b09a647650c68ddbce1cc822c64b195b514eb556
ceedar-lab <ceedar.lab@gmail.com>
2020-11-14 22:44:26 +0100

    v1.1 - Pagination added

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ additions of subtasks, deletions will be propagated at all user.
 
 __author__ = ("Clément Daroit")
 __contact__ = ("ceedar.lab@gmail.com")
-__version__ = "1.0"
-__date__ = "2020-11-09"
+__version__ = "1.1"
+__date__ = "2020-11-14"
 
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -55,7 +55,8 @@ class Task(db.Model):
     """
 
     id_task = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), nullable=False, unique=True)
+    title = db.Column(db.String(30), db.CheckConstraint(
+        'length(title) <= 20'), nullable=False, unique=True)
     creator = db.Column(
         db.Integer, db.ForeignKey('user.id_user'),
         nullable=False)
@@ -74,7 +75,8 @@ class SubTask(db.Model):
     """
 
     id_subtask = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), db.CheckConstraint(
+        'length(title) <= 25'), nullable=False)
     status = db.Column(db.Integer, default=1, nullable=False)
     id_task = db.Column(
         db.Integer, db.ForeignKey('task.id_task'),
@@ -88,6 +90,7 @@ class SubTask(db.Model):
 
 # Global
 id_task = 0
+page_num = 1
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -100,22 +103,49 @@ def widget_functionalities():
     """
     id_user = 1
     global id_task
+    global page_num
     tasks = Task.query.join(Task.users).filter(User.id_user == id_user).all()
-    subTasks = SubTask.query.filter_by(id_task=id_task).all()
+
+    def show(id, num):
+        global id_task
+        global page_num
+        id_task = id
+        page_num = num
+        subTasks = SubTask.query.filter_by(id_task=id_task).paginate(
+            page=page_num, per_page=7, error_out=False)
+        task = Task.query.get(id_task)
+        return task, subTasks
 
     if request.method == 'POST':
-        # Shows list of created tasks
+        # Shows selected task
         if 'taskList' in request.form:
-            id_task = request.form['taskList']
-            subTasks = SubTask.query.filter_by(id_task=id_task).all()
-            task = Task.query.get(id_task)
+            show = show(int(request.form['taskList']), 1)
             return render_template(
-                'index.html', task=task, subTasks=subTasks, tasks=tasks)
+                'index.html', task=show[0], subTasks=show[1], tasks=tasks)
+        # Shows previous page of subtasks
+        elif 'prevPage' in request.form:
+            show = show(id_task, int(request.form['prevPage']))
+            return render_template(
+                'index.html', task=show[0], subTasks=show[1], tasks=tasks)
+        # Shows next page of subtasks
+        elif 'nextPage' in request.form:
+            show = show(id_task, int(request.form['nextPage']))
+            return render_template(
+                'index.html', task=show[0], subTasks=show[1], tasks=tasks)
         # Allows to create new task
         elif 'add_task' in request.form:
             try:
                 task_content = request.form['add_task']
-                if task_content != "":
+                if task_content == "" or len(task_content) > 20:
+                    show = show(id_task, page_num)
+                    tasks = Task.query.filter_by(creator=id_user).order_by(
+                        Task.date_created).all()
+                    errorMessage = "Veuillez entrer un titre valide"
+                    return render_template(
+                        'index.html',
+                        task=show[0], subTasks=show[1], tasks=tasks,
+                        errorMessage=errorMessage)
+                else:
                     new_task = Task(title=task_content, creator=id_user)
                     user = User.query.get(id_user)
                     new_task.users.append(user)
@@ -123,59 +153,62 @@ def widget_functionalities():
                     db.session.commit()
                     task = Task.query.order_by(
                         Task.date_created.desc()).first()
-                    id_task = task.id_task
+                    show = show(task.id_task, 1)
                     tasks = Task.query.filter_by(creator=id_user).order_by(
                         Task.date_created).all()
                     return render_template(
-                        'index.html', task=task, tasks=tasks)
-                else:
-                    task = Task.query.get(id_task)
-                    subTasks = SubTask.query.filter_by(id_task=id_task).all()
-                    tasks = Task.query.filter_by(creator=id_user).order_by(
-                        Task.date_created).all()
-                    errorMessage = 'Veuiller entrer un titre'
-                    return render_template(
-                        'index.html',
-                        task=task, subTasks=subTasks, tasks=tasks,
-                        errorMessage=errorMessage)
+                        'index.html', task=show[0], subTasks=show[1],
+                        tasks=tasks)
             # In case of task already exists
             except Exception:
                 db.session.rollback()
-                task = Task.query.get(id_task)
-                subTasks = SubTask.query.filter_by(id_task=id_task).all()
+                show = show(id_task, page_num)
                 tasks = Task.query.filter_by(creator=id_user).order_by(
                     Task.date_created).all()
-                errorMessage = 'Cette tache existe déjà'
+                errorMessage = "Cette tâche existe déjà"
                 return render_template(
                     'index.html',
-                    task=task, subTasks=subTasks, tasks=tasks,
+                    task=show[0], subTasks=show[1], tasks=tasks,
                     errorMessage=errorMessage)
         # Allows to delete task
         elif 'remove_task' in request.form:
-            Task.query.filter_by(id_task=id_task).delete()
             SubTask.query.filter_by(id_task=id_task).delete()
+            task = Task.query.get(id_task)
+            task.users.clear()
+            db.session.delete(task)
             db.session.commit()
-            id_task = 0
             return redirect('/')
         # Allows to create new subtask
         elif 'add_subTask' in request.form:
             subTask_content = request.form['add_subTask']
-            new_subTask = SubTask(title=subTask_content, id_task=id_task)
-            db.session.add(new_subTask)
-            db.session.commit()
-            task = Task.query.get(id_task)
-            subTasks = SubTask.query.filter_by(id_task=id_task).all()
-            return render_template(
-                'index.html', task=task, subTasks=subTasks, tasks=tasks)
+            if subTask_content == "" or len(subTask_content) > 25:
+                show = show(id_task, page_num)
+                tasks = Task.query.filter_by(creator=id_user).order_by(
+                    Task.date_created).all()
+                errorMessage = "Veuillez entrer un titre valide"
+                return render_template(
+                    'index.html',
+                    task=show[0], subTasks=show[1], tasks=tasks,
+                    errorMessage=errorMessage)
+            else:
+                new_subTask = SubTask(title=subTask_content, id_task=id_task)
+                db.session.add(new_subTask)
+                db.session.commit()
+                last_page = SubTask.query.filter_by(id_task=id_task).paginate(
+                    page=page_num, per_page=7, error_out=False)
+                show = show(id_task, last_page.pages)
+                return render_template(
+                    'index.html', task=show[0], subTasks=show[1], tasks=tasks)
         # Allows to delete subtask
         elif 'remove_subTask' in request.form:
             id_subTask = request.form['id_subTask']
             SubTask.query.filter_by(id_subtask=id_subTask).delete()
             db.session.commit()
-            task = Task.query.get(id_task)
-            subTasks = SubTask.query.filter_by(id_task=id_task).all()
+            last_page = SubTask.query.filter_by(id_task=id_task).paginate(
+                page=page_num, per_page=7, error_out=False)
+            show = show(id_task, last_page.pages)
             return render_template(
-                'index.html', task=task, subTasks=subTasks, tasks=tasks)
+                'index.html', task=show[0], subTasks=show[1], tasks=tasks)
         # Allows to edit subtask
         elif 'edit_subTask' in request.form:
             subTask_content = request.form['edit_subTask']
@@ -186,37 +219,32 @@ def widget_functionalities():
             SubTask.query.filter_by(id_subtask=id_subTask).update(
                 {SubTask.status: subTask_state})
             db.session.commit()
-            task = Task.query.get(id_task)
-            subTasks = SubTask.query.filter_by(id_task=id_task).all()
+            show = show(id_task, page_num)
             return render_template(
-                'index.html', task=task, subTasks=subTasks, tasks=tasks)
+                'index.html', task=show[0], subTasks=show[1], tasks=tasks)
         # Allows to share a task with another user
         elif 'add_assignee' in request.form:
             try:
                 new_assignee = request.form['add_assignee']
                 new_assignee = User.query.filter_by(name=new_assignee).all()
                 new_assignee = new_assignee[0]
-                task = Task.query.get(id_task)
-                task.users.append(new_assignee)
-                db.session.add(task)
+                show = show(id_task, page_num)
+                show[0].users.append(new_assignee)
+                db.session.add(show[0])
                 db.session.commit()
-                subTasks = SubTask.query.filter_by(id_task=id_task).all()
-                errorMessage = 'utilisateur ajouté'
+                errorMessage = "Utilisateur ajouté"
                 return render_template(
-                    'index.html', task=task, subTasks=subTasks, tasks=tasks,
+                    'index.html', task=show[0], subTasks=show[1], tasks=tasks,
                     errorMessage=errorMessage)
             # If user doesn't exist
             except Exception:
                 db.session.rollback()
-                task = Task.query.get(id_task)
-                subTasks = SubTask.query.filter_by(id_task=id_task).all()
-                tasks = Task.query.filter_by(creator=id_user).order_by(
-                    Task.date_created).all()
-                errorMessage = 'utilisateur inexistant'
+                show = show(id_task, page_num)
+                errorMessage = "Utilisateur inexistant"
                 return render_template(
-                    'index.html', task=task, subTasks=subTasks, tasks=tasks,
+                    'index.html', task=show[0], subTasks=show[1], tasks=tasks,
                     errorMessage=errorMessage)
     else:
-        task = Task.query.get(id_task)
+        show = show(0, 1)
         return render_template(
-            'index.html', task=task, subTasks=subTasks, tasks=tasks)
+            'index.html', task=show[0], subTasks=show[1], tasks=tasks)
